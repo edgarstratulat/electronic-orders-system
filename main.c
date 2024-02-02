43fcae3e5317551c2ab72ae2c0c0aa868f631459
Edgar Sousa <138028818+edgarstratulat@users.noreply.github.com>
2024-02-02 19:39:01 +0000

    Update 1.2
    Registration option 1 completed!
    
    The log validates this data:
    
    I used do while
    
    - The username must contain only letters.
    - The age must be greater than zero.
    - The e-mail address must contain @ .
    - The password and password confirmation must be the same.
    - The mobile number must contain only numbers (not letters).

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 typedef struct
 {
@@ -6,13 +8,119 @@ typedef struct
     int idade;
     char email[60];
     char passe[60];
+    char confirmacaopasse[60];
     char tel[9];
 } Utilizador;
 
-// Array
-Utilizador utilizadores[];
+// Array utilizadores
+Utilizador utilizadores[10];
 
-void registo();
+// Função registo
+void registo(Utilizador utilizadores[]);
+
+// Função login
+void login(Utilizador utilizadores[]);
+
+int validarNome(const char *letra)
+{
+    while (*letra)
+    {
+        if (!isalpha(*letra))
+        {
+            return 0;
+        }
+        letra++;
+    }
+    return 1;
+}
+
+int validarNumero(const char *numero)
+{
+    while (*numero)
+    {
+        if (!isdigit(*numero))
+        {
+            return 0;
+        }
+        numero++;
+    }
+    return 1;
+}
+
+void registo(Utilizador utilizadores[])
+{
+    Utilizador NovoUtilizador;
+    fflush(stdin);
+
+    // Validação para o nome
+    do
+    {
+        printf("Nome de Utilizador: ");
+        scanf("%s", NovoUtilizador.user);
+        if (!validarNome(NovoUtilizador.user))
+        {
+            printf("O nome de utilizador só deve conter letras!\n");
+        }
+    } while (!validarNome(NovoUtilizador.user));
+
+    // Validação para a idade
+    do
+    {
+        printf("Idade: ");
+        scanf("%d", &NovoUtilizador.idade);
+        if (NovoUtilizador.idade <= 0)
+        {
+            printf("Idade não pode ser menor que 0\n");
+        }
+    } while (NovoUtilizador.idade <= 0);
+
+    // Validação para o email
+    do
+    {
+        printf("Email: ");
+        scanf("%s", NovoUtilizador.email);
+        if (!strchr(NovoUtilizador.email, '@'))
+        {
+            printf("O email deve conter @\n");
+        }
+    } while (!strchr(NovoUtilizador.email, '@'));
+
+    // Validar número
+    do
+    {
+        printf("Número de Telemóvel: ");
+        scanf("%s", NovoUtilizador.tel);
+        if (!validarNumero(NovoUtilizador.tel))
+        {
+            printf("O número de telemóvel só deve conter números\n");
+        }
+    } while (!validarNumero(NovoUtilizador.tel));
+
+    // Confirmação palavra-passe
+    do
+    {
+        printf("Palavra-passe: ");
+        scanf("%s", NovoUtilizador.passe);
+        printf("Confirmar Palavra-passe: ");
+        scanf("%s", NovoUtilizador.confirmacaopasse);
+        if (strcmp(NovoUtilizador.passe, NovoUtilizador.confirmacaopasse) != 0)
+        {
+            printf("As palavras passes não coincidem!\n");
+        }
+    } while (strcmp(NovoUtilizador.passe, NovoUtilizador.confirmacaopasse) != 0);
+
+    printf("Registo concluído com sucesso!");
+}
+
+void login(Utilizador utilizadores[])
+{
+    Utilizador UtilizadorExistente;
+
+    printf("Email:\n");
+    scanf("%s", UtilizadorExistente.email);
+    printf("Palavra-passe:\n");
+    scanf("%s", UtilizadorExistente.passe);
+}
 
 int main()
 {
@@ -20,7 +128,7 @@ int main()
     int opcao;
 
     printf("Bem-vindo a loja de eletrónicos!\n\n");
-    printf("Selecione uma opção!\n\n");
+    printf("Selecione uma opção!\n");
     printf("1. Registar\n");
     printf("2. Entrar\n");
     printf("3. Sair\n");
@@ -32,11 +140,13 @@ int main()
     {
     case 1:
         printf("Opção 1 selecionada!\n\n");
-        registo();
+        registo(utilizadores);
+
         break;
 
     case 2:
         printf("Opção 2 selecionada!\n");
+        login(utilizadores);
         break;
 
     case 3:
@@ -47,24 +157,4 @@ int main()
         printf("Opção inválida!");
         break;
     }
-}
-
-// Função registo
-void registo()
-{
-
-    Utilizador NovoUtilizador;
-
-    printf("Nome de Utilizador:\n");
-    scanf("%s", &NovoUtilizador.user);
-    printf("Idade:\n");
-    scanf("%d", &NovoUtilizador.idade);
-    printf("Email:\n");
-    scanf("%s", &NovoUtilizador.email);
-    printf("Número de Telefone:\n");
-    scanf("%s", &NovoUtilizador.tel);
-    printf("Palavra-passe:\n");
-    scanf("%s", &NovoUtilizador.passe);
-
-    printf("Registo concluído com sucesso!");
 }
